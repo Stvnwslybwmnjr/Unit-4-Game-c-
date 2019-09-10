5b7700843d797368e224013c27768507c3178c38
Steven Bowman <stevenbowmanjr@gmail.com>
2019-09-10 14:18:08 -0700

    buttons added buttons that add, reset function. Conditional set at the end but it doesn't work because of scoping issues

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,31 +1,76 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
 
-let goal = Math.floor(Math.random()*101+19)
-console.log("goal " + goal)
+    let goal = Math.floor(Math.random() * 101 + 19);
+    console.log("goal " + goal);
 
+    $("#goal").text(goal);
 
+    let button1 = Math.floor(Math.random() * 11 + 1);
+    console.log("b1 " + button1);
+    let button2 = Math.floor(Math.random() * 11 + 1);
+    let button3 = Math.floor(Math.random() * 11 + 1);
+    let button4 = Math.floor(Math.random() * 11 + 1);
 
-let button1 = Math.floor(Math.random()*11+1)
-console.log("b1 " + button1);
-let button2 = Math.floor(Math.random()*11+1)
-let button3 = Math.floor(Math.random()*11+1)
-let button4 = Math.floor(Math.random()*11+1)
+    console.log("b2 " + button2);
+    console.log("b3 " + button3);
+    console.log("b4 " + button4);
 
-console.log("b2 " + button2);
-console.log("b3 " + button3);
-console.log("b4 " + button4);
+    let sum = 0;
+    let wins = 0;
+    let loses = 0;
+    var displaywins = $("#wins");
+    var displayloses = $("#loses");
+    var displaysum = $("#sum");
 
-let wins = 0;
-let loses = 0;
-var displaywins = $("#wins");
-var displayloses = $("loses");
+    displaywins.text(wins);
+    displayloses.text(loses);
 
-displaywins.text(wins);
-displayloses.text(loses);
+    function reset(){
+        goal = Math.floor(Math.random() * 101 + 19);
+        button1 = Math.floor(Math.random() * 11 + 1);
+        button2 = Math.floor(Math.random() * 11 + 1);
+        button3 = Math.floor(Math.random() * 11 + 1);
+        button4 = Math.floor(Math.random() * 11 + 1);
+        displaysum.text(sum);
+        $("#goal").text(goal);
+        
+    }
 
-$(".btn1").on("click", function() {
-    
-})
+    $(".btn1").on("click", function () {
+        sum += button1;
+        displaysum.text(sum);
+        console.log(button1);
+        return sum;
+    });
 
+    $(".btn2").on("click", function () {
+        sum = sum + button2;
+        displaysum.text(sum);
+        console.log(button2);
+        return sum;
+    });
+
+    $(".btn3").on("click", function () {
+        sum = sum + button3;
+        displaysum.text(sum);
+        console.log(button3);
+        return sum;
+    });
+
+    $(".btn4").on("click", function () {
+        sum = sum + button4;
+        displaysum.text(sum)
+        console.log(button4);
+        var globalsum = sum
+    })
+
+    console.log("endgame " + globalsum)
+    console.log("endgame " + goal)
+    if (sum == goal) {
+        alert("you won");
+    }
+    else if (sum > goal)
+        alert("you lost");
 });
+
